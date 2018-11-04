feccbd6c8f526637cecb1aed80e889dd5c6c3085
zky <1964483239@qq.com>
2018-11-05 01:06:53 +0800

    advanced features about filter in python and about all the example for this toturials.

--- study/pythonMethod/pythonAdvancdeFeatures20181104.py
+++ study/pythonMethod/pythonAdvancdeFeatures20181104.py
@@ -139,4 +139,112 @@ print(list(map(normalize, ['abbAt', 'appLE', 'gooGLE'])))  # => ['Abbat', 'Apple
 
 L1 = ['adam', 'LISA', 'barT']
 L2 = list(map(normalize, L1))
-print(L2) # =>['Adam', 'Lisa', 'Bart']
+print(L2)  # =>['Adam', 'Lisa', 'Bart']
+
+# Python提供的sum()函数可以接受一个list并求和，请编写一个prod()函数，可以接受一个list并利用reduce()求积：
+from functools import reduce
+
+
+def okGo(x, y):
+    return x * y
+
+
+print(reduce(okGo, [1, 2, 3]))  # =>6
+# 这个没有达到要求，再思考思考
+
+
+# 利用map和reduce编写一个str2float函数，把字符串'123.456'转换成浮点数123.456：
+
+from functools import reduce
+
+CHAR_TO_FLOAT = {'0': 0, '1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9, '.': -1}
+
+
+def str2num(s):
+    nums = map(lambda ch: CHAR_TO_FLOAT[ch], s)
+    point = 0
+
+    def to_float(f, n):
+        nonlocal point
+        if n == -1:
+            point = 1
+            return f
+        if point == 0:
+            return f * 10 + n
+        else:
+            point = point * 10
+            return f + n / point
+
+    return reduce(to_float, nums, 0.0)
+
+
+print(str2num('1.234'))  # =>1.234
+print(str2num('.234'))  # =>0.234
+print(str2num('0.234'))  # =>0.234
+print(str2num('0'))  # =>0.0
+print(str2num('1.23400'))  # =>1.234
+
+
+
+
+
+# example:
+
+from functools import reduce
+
+CHAR_TO_INT = {
+    '0': 0,
+    '1': 1,
+    '2': 2,
+    '3': 3,
+    '4': 4,
+    '5': 5,
+    '6': 6,
+    '7': 7,
+    '8': 8,
+    '9': 9
+}
+
+def str2int(s):
+    ints = map(lambda ch: CHAR_TO_INT[ch], s)
+    return reduce(lambda x, y: x * 10 + y, ints)
+
+print(str2int('0'))
+print(str2int('12300'))
+print(str2int('0012345'))
+
+CHAR_TO_FLOAT = {
+    '0': 0,
+    '1': 1,
+    '2': 2,
+    '3': 3,
+    '4': 4,
+    '5': 5,
+    '6': 6,
+    '7': 7,
+    '8': 8,
+    '9': 9,
+    '.': -1
+}
+
+def str2float(s):
+    nums = map(lambda ch: CHAR_TO_FLOAT[ch], s)
+    point = 0
+    def to_float(f, n):
+        nonlocal point
+        if n == -1:
+            point = 1
+            return f
+        if point == 0:
+            return f * 10 + n
+        else:
+            point = point * 10
+            return f + n / point
+    return reduce(to_float, nums, 0.0)
+
+print(str2float('0'))
+print(str2float('123.456'))
+print(str2float('123.45600'))
+print(str2float('0.1234'))
+print(str2float('.1234'))
+print(str2float('120.0034'))
