12517f195702f9648528df0f635f10c940a0b216
zhouky <zhouky@hongtiantao.com>
2018-12-13 16:43:16 +0800

    python about progress.

--- study/pythonOop/pythonOOP20181203.py
+++ study/pythonOop/pythonOOP20181203.py
@@ -108,22 +108,22 @@ print('Process (%s) start...' % os.getppid()) # =>Process (5548) start...
 
 from multiprocessing import Pool
 import os,time,random
-def long_time_task(name):
-    print('Run task %s (%s)'%(name,os.getpid()))
-    start = time.time()
-    time.sleep(random.random()*3)
-    end = time.time()
-    print('Task %s run %0.2f seconds'%(name,(end-start)))
-
-if __name__ == '__main__':
-    print('Parent process %s' % os.getpid())
-    p = Pool(4)
-    for i in range(5):
-        p.apply_async(long_time_task,args=(i,))
-    print('Waitting for all subprocess done...')
-    p.close()
-    p.join()
-    print('All subprocess done.')
+# def long_time_task(name):
+#     print('Run task %s (%s)'%(name,os.getpid()))
+#     start = time.time()
+#     time.sleep(random.random()*3)
+#     end = time.time()
+#     print('Task %s run %0.2f seconds'%(name,(end-start)))
+#
+# if __name__ == '__main__':
+#     print('Parent process %s' % os.getpid())
+#     p = Pool(4)
+#     for i in range(5):
+#         p.apply_async(long_time_task,args=(i,))
+#     print('Waitting for all subprocess done...')
+#     p.close()
+#     p.join()
+#     print('All subprocess done.')
 
 
 
@@ -159,3 +159,14 @@ if __name__ == '__main__':
 #
 # 由于Pool的默认大小是CPU的核数，如果你不幸拥有8核CPU，你要提交至少9个子进程才能看到上面的等待效果。
 
+# 子进程
+# 很多时候，子进程并不是自身，而是一个外部进程。我们创建了子进程后，还需要控制子进程的输入和输出。
+#
+# subprocess模块可以让我们非常方便地启动一个子进程，然后控制其输入和输出。
+#
+# 下面的例子演示了如何在Python代码中运行命令nslookup www.python.org，这和命令行直接运行的效果是一样的：
+
+import subprocess
+print('$ nslookup www.python.org')
+r = subprocess.call(['nslookup','www.python.org'])
+print('Exit code:',r)
