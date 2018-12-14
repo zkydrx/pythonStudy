c3632f2bd9b4bcf0bc4d9e00dc80a4539e1c1c4d
zhouky <zhouky@hongtiantao.com>
2018-12-14 10:32:28 +0800

    python about progress.

--- study/pythonOop/pythonOOP20181203.py
+++ study/pythonOop/pythonOOP20181203.py
@@ -167,6 +167,90 @@ import os,time,random
 # 下面的例子演示了如何在Python代码中运行命令nslookup www.python.org，这和命令行直接运行的效果是一样的：
 
 import subprocess
-print('$ nslookup www.python.org')
-r = subprocess.call(['nslookup','www.python.org'])
-print('Exit code:',r)
+# print('$ nslookup www.python.org')
+# r = subprocess.call(['nslookup','www.python.org'])
+# print('Exit code:',r)
+
+# ��Ȩ��Ӧ��:
+# ������:  UnKnown
+# Address:  192.168.50.10
+#
+# ����:    dualstack.python.map.fastly.net
+# Addresses:  2a04:4e42:1a::223
+# 	  151.101.108.223
+# Aliases:  www.python.org
+
+# 如果子进程还需要输入，则可以通过communicate()方法输入：
+# import subprocess
+# print('$ nslookup')
+# p = subprocess.Popen(['nslookup'],stdin=subprocess.PIPE,stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+# output,err = p.communicate(b'set q=mx\npython.org\nexit\n')
+# print(output.decode('utf-8'))
+# print('Exit code:',p.returncode)
+
+# 上面的代码相当于在命令行执行命令nslookup，然后手动输入：
+
+# set q=mx
+# python.org
+# exit
+# 运行结果如下：
+#
+# $ nslookup
+# Server:        192.168.19.4
+# Address:    192.168.19.4#53
+#
+# Non-authoritative answer:
+# python.org    mail exchanger = 50 mail.python.org.
+#
+# Authoritative answers can be found from:
+# mail.python.org    internet address = 82.94.164.166
+# mail.python.org    has AAAA address 2001:888:2000:d::a6
+#
+#
+# Exit code: 0
+# 进程间通信
+# Process之间肯定是需要通信的，操作系统提供了很多机制来实现进程间的通信。Python的multiprocessing模块包装了底层的机制，提供了Queue、Pipes等多种方式来交换数据。
+#
+# 我们以Queue为例，在父进程中创建两个子进程，一个往Queue里写数据，一个从Queue里读数据：
+from multiprocessing import Process,Queue
+import os,time,random
+# 写数据进程执行的代码
+def write(q):
+    print('Process to write:%s'% os.getpid())
+    for value in ['A','B','C']:
+        print('Put %s to queue...' % value)
+        q.put(value)
+        time.sleep(random.random())
+
+# 读取数据进程执行的代码
+def read(q):
+    print('Process to read: %s'% os.getpid())
+    while True:
+        value = q.get(True)
+        print('Get %s from queue:' % value)
+
+if __name__ == '__main__':
+    # 以父进程创建queue，并传递给各个子进程
+    q = Queue()
+    pw = Process(target=write,args=(q,))
+    pr = Process(target=read,args=(q,))
+    # 启动子进程pw，写入：
+    pw.start()
+    # 启动子进程pr,读取：
+    pr.start()
+    # 等待pw结束
+    pw.join()
+    # pr进程是死循环，无法等待其结束，只能强行终止
+    pr.terminate()
+
+
+
+
+# Process to write:21988
+# Put A to queue...
+# Process to read: 16904
+# Get A from queue:
+# Put B to queue...
+# Get B from queue:
+# Put C to queue...
+# Get C from queue:
