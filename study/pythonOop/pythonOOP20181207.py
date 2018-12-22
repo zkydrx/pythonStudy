cec3d082d58cb31b64d3091ccd5788f6318f6d60
zky <1964483239@qq.com>
2018-12-22 22:44:03 +0800

    python about distributed.

--- study/pythonOop/pythonOOP20181207.py
+++ study/pythonOop/pythonOOP20181207.py
@@ -13,3 +13,12 @@
 
 
 import random, time, queue
+from multiprocessing.managers import BaseManager
+# 发送任务的队列：
+task_queue=queue.Queue()
+# 接收结果的队列：
+result_queue=queue.Queue()
+
+# 从BaseManager继承的QueueManager:
+class QueueManager(BaseManager):
+    pass
