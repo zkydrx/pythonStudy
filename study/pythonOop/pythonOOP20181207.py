7d0a63f2094bb5d7cca020ba355f014243572a3c
zky <1964483239@qq.com>
2018-12-23 23:48:20 +0800

    python about distributed.

--- study/pythonOop/pythonOOP20181207.py
+++ study/pythonOop/pythonOOP20181207.py
@@ -22,3 +22,30 @@ result_queue=queue.Queue()
 # 从BaseManager继承的QueueManager:
 class QueueManager(BaseManager):
     pass
+
+# 把两个Queue都注册到网络上, callable参数关联了Queue对象:
+
+QueueManager.register('get_task_queue',callable=lambda :task_queue)
+QueueManager.register('get_result_queue',callable=lambda :result_queue)
+
+# 绑定端口5000, 设置验证码'abc':
+
+manager = QueueManager(address=('',5000),authkey=b'abc')
+# 启动Queue
+manager.start()
+#获得通过网络访问的Queue对象：
+task=manager.get_task_queue()
+result = manager.get_result_queue()
+
+#放几个任务进去
+for i in range(10):
+    n = random.randint(1,10000)
+    r = result.get(timeout=10)
+    print('Result:%s'%r)
+# 关闭：
+manager.shutdown()
+print('master exit.')
+
+# 请注意，当我们在一台机器上写多进程程序时，创建的Queue可以直接拿来用，但是，在分布式多进程环境下，添加任务到Queue不可以直接对原始的task_queue进行操作，那样就绕过了QueueManager的封装，必须通过manager.get_task_queue()获得的Queue接口添加。
+#
+# 然后，在另一台机器上启动任务进程（本机上启动也可以）：
